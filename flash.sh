b8b94cabe3a3b3d47f4ddc39dfedb0bf500893b2
wykys <vvykys@gmail.com>
2021-11-12 20:31:18 +0100

    Created flash script

---
+++ flash.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# wykys 2021
+# Lazy Firmware Update
+
+RESET_PIN 25
+BOOT0_PIN 23
+
+GPIO_DRIVER="./wiringOP/gpio/gpio"
+
+DEVICE="0483:df11"
+FIRMWARE="~/klipper/out/klipper.bin"
+
+###############################################################################
+
+delay () {
+    sleep 0.5
+}
+
+gpio_init() {
+    $GPIO_DRIVER mode $RESET_PIN OUTPUT
+    $GPIO_DRIVER mode $BOOT0_PIN OUTPUT
+    delay
+}
+
+reset_mcu () {
+    $GPIO_DRIVER write $RESET_PIN 0
+    delay
+}
+
+run_mcu () {
+    $GPIO_DRIVER write $RESET_PIN 1
+    delay
+}
+
+boot_dfu () {
+    $GPIO_DRIVER write $BOOT0_PIN 1
+    delay
+}
+
+boot_flash () {
+    $GPIO_DRIVER write $BOOT0_PIN 0
+    delay
+}
+
+###############################################################################
+
+flash () {
+    dfu-util --device $DEVICE -D $FIRMWARE --alt 0 -s 0x08000000
+}
+
+main () {
+    gpio_init
+    reset_mcu
+    boot_dfu
+    run_mcu
+    flash
+    reset_mcu
+    boot_flash
+    run_mcu
+}
+
+###############################################################################
+
+main
--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# wykys 2021
+# Lazy Firmware Update
 
 cd wiringOP
 ./build clean
